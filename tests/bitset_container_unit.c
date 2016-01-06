40e71cc7e018d0ff3fa7f9986b1c2fbd05b1bb16
Daniel Lemire <lemire@gmail.com>
2016-01-06 12:15:52 -0500

    Added unit test.

--- tests/bitset_container_unit.c
+++ tests/bitset_container_unit.c
@@ -9,6 +9,7 @@
 
 #include "containers/bitset.h"
 
+// returns 0 on error, 1 if ok.
 int set_get_test() {
     bitset_container_t* B = bitset_container_create();
     int x;
@@ -56,8 +57,63 @@ int set_get_test() {
     return 1;
 }
 
+// returns 0 on error, 1 if ok.
+int and_or_test() {
+	bitset_container_t* B1 = bitset_container_create();
+	bitset_container_t* B2 = bitset_container_create();
+	bitset_container_t* BI = bitset_container_create();
+	bitset_container_t* BO = bitset_container_create();
+
+	int x, c, ci, co;
+	printf("[%s] %s\n", __FILE__, __func__);
+    if ((B1 == NULL) || (B2 == NULL) || (BO == NULL) || (BI == NULL)) {
+	        printf("Bug %s, line %d \n", __FILE__, __LINE__);
+	        return 0;
+	}
+	for (x = 0; x < (1<<16); x += 3) {
+	        bitset_container_set(B1, (uint16_t)x);
+	        bitset_container_set(BI, (uint16_t)x);
+
+	}
+	for (x = 0; x < (1<<16); x += 62) {// important: 62 is not divisible by 3
+	        bitset_container_set(B2, (uint16_t)x);
+	        bitset_container_set(BI, (uint16_t)x);
+	}
+	for (x = 0; x < (1<<16); x += 62*3) {
+	        bitset_container_set(BO, (uint16_t)x);
+	}
+	// we interleave O and I on purpose (to trigger bugs!)
+	ci = bitset_container_compute_cardinality(BO);// expected intersection
+	co = bitset_container_compute_cardinality(BI);// expected union
+	bitset_container_and_nocard(B1,B2, BI);
+	c = bitset_container_compute_cardinality(BI);
+	if(c != ci) {
+        printf("Bug %s, line %d \n", __FILE__, __LINE__);
+        return 0;
+	}
+	c = bitset_container_and(B1,B2, BI);;
+	if(c != ci) {
+	    printf("Bug %s, line %d \n", __FILE__, __LINE__);
+	    return 0;
+	}
+	bitset_container_or_nocard(B1,B2, BO);
+	c = bitset_container_compute_cardinality(BO);
+	if(c != co) {
+	    printf("Bug %s, line %d \n", __FILE__, __LINE__);
+	    return 0;
+	}
+	c = bitset_container_or(B1,B2, BO);
+	if(c != co) {
+        printf("Bug %s, line %d \n", __FILE__, __LINE__);
+        return 0;
+	}
+	return 1;
+}
+
 int main() {
     if (!set_get_test()) return -1;
+    if (!and_or_test()) return -1;
+
     printf("[%s] your code might be ok.\n", __FILE__);
     return 0;
 }
