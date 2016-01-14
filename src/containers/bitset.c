fd9381fd7ea76de1298867733b9deb3819f28477
Daniel Lemire <lemire@gmail.com>
2016-01-14 14:31:31 -0500

    Tentatively throwing in Nate's hamming weight approach.

--- src/containers/bitset.c
+++ src/containers/bitset.c
@@ -57,6 +57,11 @@ bool bitset_container_get(const bitset_container_t *bitset, uint16_t pos) {
     return (word >> (pos & 63)) & 1;
 }
 
+
+#define USEPOPCNT
+
+#ifdef USEPOPCNT
+
 /* Get the number of bits set (force computation) */
 int bitset_container_compute_cardinality(const bitset_container_t *bitset) {
     const uint64_t *array = bitset->array;
@@ -69,6 +74,33 @@ int bitset_container_compute_cardinality(const bitset_container_t *bitset) {
     }
     return sum;
 }
+
+#else
+
+/* Get the number of bits set (force computation) */
+int bitset_container_compute_cardinality(const bitset_container_t *bitset) {
+    const uint64_t *array = bitset->array;
+    // these are precomputed hamming weights (weight(0), weight(1)...)
+    const __m256i shuf = _mm256_setr_epi8(0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
+       0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4);
+    const __m256i  mask = _mm256_set1_epi8(0x0f); // low 4 bits of each byte
+    __m256i total = _mm256_setzero_si256();
+    __m256i zero = _mm256_setzero_si256();
+    for (int i = 0; i < BITSET_CONTAINER_SIZE_IN_WORDS; i += 4) {
+        __m256i ymm1 = _mm256_lddqu_si256((const __m256i *)(array + i));
+        __m256i ymm2 = _mm256_srli_epi32(ymm1,4); // shift right, shiftingin zeroes
+        ymm1 = _mm256_and_si256(ymm1,mask); // contains even 4 bits
+        ymm2 = _mm256_and_si256(ymm2,mask); // contains odd 4 bits
+        ymm1 = _mm256_shuffle_epi8(shuf,ymm1);// use table look-up to sum the 4 bits
+        ymm2 = _mm256_shuffle_epi8(shuf,ymm2);
+        ymm1 = _mm256_add_epi8(ymm1,ymm2);// each byte contains weight of input byte (values are in [0,8))
+        ymm1 = _mm256_sad_epu8(zero,ymm1);// produces 4 64-bit counters (having values in [0,32))
+        total= _mm256_add_epi32(ymm1,total); // add the 4 64-bit counters to previous counter
+    }
+    return _mm256_extract_epi64(total,0)+_mm256_extract_epi64(total,1)+_mm256_extract_epi64(total,2)+_mm256_extract_epi64(total,3);
+}
+#endif
+
 
 #ifdef USEAVX
 
