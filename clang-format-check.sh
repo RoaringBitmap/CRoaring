7ec054e780038a34fbd65e13321b0fa39ff2690a
Daniel Lemire <lemire@gmail.com>
2016-02-24 16:44:07 -0500

    Adding script to check format

---
+++ clang-format-check.sh
@@ -0,0 +1,18 @@
+STYLE=$(which clang-format)
+if [ $? -ne 0 ]; then
+	echo "clang-format not installed. Unable to check source file format policy." >&2
+	exit 1
+fi
+RE=0
+ALLFILES=$(find -regex '.*/.*\.\(c\|hh\|cc\|cpp\|h\)$')
+for FILE in $ALLFILES; do
+  $STYLE $FILE | cmp -s $FILE -
+  if [ $? -ne 0 ]; then
+        echo "$FILE does not respect the coding style." >&2
+        echo "consider typing $STYLE -i $FILE to fix the problem." >&2
+        echo
+        RE=1
+  fi 
+done
+
+exit $RE
