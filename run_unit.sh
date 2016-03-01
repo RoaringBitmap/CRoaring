aaff21bd360070a686ff94d09892f0530e0a5713
Daniel Lemire <lemire@gmail.com>
2016-03-01 11:32:35 -0500

    Adding a final printout to confirm that all is right.

--- run_unit.sh
+++ run_unit.sh
@@ -24,6 +24,8 @@ function main() {
   echo -e " \x1B[0;32mTesting AVX version.\x1B[0m "
   make --silent NOAVXTUNING=0 clean all
   run_tests
+
+  echo -e "\n\n \x1B[0;31m[\x1B[0m \x1B[0;32mAll tests clear.\x1B[0m \x1B[0;31m]\x1B[0m \n\n"
 }
 
 main
