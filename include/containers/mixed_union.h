8784140087bd4249bd9b1ccae6d5217d2b1a7acd
Daniel Lemire <lemire@gmail.com>
2016-03-08 23:07:54 -0500

    Reformatting.

--- include/containers/mixed_union.h
+++ include/containers/mixed_union.h
@@ -10,13 +10,10 @@
 #include "bitset.h"
 #include "run.h"
 
-
 /* Compute the union of src_1 and src_2 and write the result to
  * dst. It is allowed for src_1 to be dst.   */
 void array_bitset_container_union(const array_container_t *src_1,
                                   const bitset_container_t *src_2,
-								  bitset_container_t *dst);
-
-
+                                  bitset_container_t *dst);
 
 #endif /* INCLUDE_CONTAINERS_MIXED_INTERSECTION_H_ */
