1240b2b0283998d55dacd164d4418a5cb0f0b196
Daniel Lemire <lemire@gmail.com>
2016-03-08 23:07:22 -0500

    Introducing a conditional bitset-bitset intersection function.

--- include/containers/mixed_intersection.h
+++ include/containers/mixed_intersection.h
@@ -10,15 +10,21 @@
 #include "bitset.h"
 #include "run.h"
 
-
 /* Compute the intersection of src_1 and src_2 and write the result to
- * dst. It is allowed for dst to be equal to src_1. */
+ * dst. It is allowed for dst to be equal to src_1. We assume that dst is a
+ * valid container. */
 void array_bitset_container_intersection(const array_container_t *src_1,
-                                  const bitset_container_t *src_2,
-                                  array_container_t *dst);
+                                         const bitset_container_t *src_2,
+                                         array_container_t *dst);
 
-
-
-
+/*
+ * Compute the intersection between src_1 and src_2 and write the result
+ * to *dst. If the return function is true, the result is a bitset_container_t
+ * otherwise is a array_container_t. We assume that dst is not pre-allocated. In
+ * cast of failure, *dst will be NULL.
+ */
+bool bitset_bitset_container_intersection(const bitset_container_t *src_1,
+                                          const bitset_container_t *src_2,
+                                          void **dst);
 
 #endif /* INCLUDE_CONTAINERS_MIXED_INTERSECTION_H_ */
--- src/containers/mixed_intersection.c
+++ src/containers/mixed_intersection.c
@@ -23,3 +23,40 @@ void array_bitset_container_intersection(const array_container_t *src_1,
         }
     }
 }
+
+/*
+ * Compute the intersection between src_1 and src_2 and write the result
+ * to *dst. If the return function is true, the result is a bitset_container_t
+ * otherwise is a array_container_t.
+ */
+bool bitset_bitset_container_intersection(const bitset_container_t *src_1,
+                                          const bitset_container_t *src_2,
+                                          void **dst) {
+    const int newCardinality = bitset_container_and_justcard(src_1, src_2);
+    if (newCardinality > DEFAULT_MAX_SIZE) {
+        *dst = bitset_container_create();
+        if (*dst != NULL) {
+            bitset_container_and_nocard(src_1, src_2, *dst);
+            ((bitset_container_t *)*dst)->cardinality = newCardinality;
+        }
+        return true;  // it is a bitset
+    }
+    *dst = array_container_create_given_capacity(newCardinality);
+    if (*dst != NULL) {
+        ((array_container_t *)*dst)->cardinality = newCardinality;
+        int outpos = 0;
+        uint16_t *out = ((array_container_t *)*dst)->array;
+        for (int i = 0; i < BITSET_CONTAINER_SIZE_IN_WORDS; ++i) {
+            // todo: vectorize!
+            uint64_t w = ((bitset_container_t *)src_1)->array[i] &
+                         ((bitset_container_t *)src_2)->array[i];
+            while (w != 0) {
+                uint64_t t = w & -w;
+                int r = __builtin_ctzl(w);
+                out[outpos++] = i * 64 + r;
+                w ^= t;
+            }
+        }
+    }
+    return false;  // not a bitset
+}
