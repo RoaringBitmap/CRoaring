e15ce41a230cc234be9b09b6f6806d886937fcaf
Daniel Lemire <lemire@gmail.com>
2016-09-12 12:02:59 -0400

    Fixing pre-release script

--- tools/prereleasetests.sh
+++ tools/prereleasetests.sh
@@ -2,14 +2,18 @@
 set -e
 MAINDIR=$( git rev-parse --show-toplevel)
 echo $MAINDIR
+echo "cleaning main directory"
+rm -r -f $MAINDIR/CMakeFiles 
+rm -f $MAINDIR/CMakeCache.txt
+
 # the directory of the script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # the temp directory used, within $DIR
-WORK_DIR=`mktemp -d -p "$DIR"`
+WORK_DIR=`mktemp -d `
 
 # deletes the temp directory
 function cleanup {
-  rm -rf "$WORK_DIR"
+  #rm -rf "$WORK_DIR"
   echo "Deleted temp working directory $WORK_DIR"
 }
 
@@ -22,21 +26,21 @@ echo "working in " $PWD
 
 echo "testing debug"
 set -x          # activate debugging from here
-mkdir -p debug && cd debug && cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZE=ON $MAINDIR && make && make test
+cd $WORK_DIR && mkdir debug && cd debug && cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZE=ON $MAINDIR && make && make test
 set +x
 
 echo "testing debug no x64"
 set -x          # activate debugging from here
-mkdir -p debugnox64 && cd debugnox64 && cmake -DDISABLE_X64=ON -DCMAKE_BUILD_TYPE=Debug -DSANITIZE=ON $MAINDIR && make && make test
+cd $WORK_DIR && mkdir debugnox64 && cd debugnox64 && cmake -DDISABLE_X64=ON -DCMAKE_BUILD_TYPE=Debug -DSANITIZE=ON $MAINDIR && make && make test
 set +x
 
 echo "testing release"
 set -x          # activate debugging from here
-mkdir -p release && cd release && cmake $MAINDIR && make && make test
+cd $WORK_DIR && mkdir release && cd release && cmake $MAINDIR && make && make test
 set +x
 
 echo "testing release no x64"
 set -x          # activate debugging from here
-mkdir -p releasenox64 && cd releasenox64 && cmake -DDISABLE_X64=ON $MAINDIR  && make && make test
+cd $WORK_DIR && mkdir releasenox64 && cd releasenox64 && cmake -DDISABLE_X64=ON $MAINDIR  && make && make test
 set +x
 
