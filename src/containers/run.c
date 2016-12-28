4297db1ff6616438ebbcb1c3f9fc9a9f3c1fed5e
Daniel Lemire <lemire@gmail.com>
2016-12-28 17:51:47 -0500

    Fix for https://github.com/RoaringBitmap/CRoaring/issues/71

--- src/containers/run.c
+++ src/containers/run.c
@@ -134,7 +134,7 @@ int run_container_cardinality(const run_container_t *run) {
         }
         // a store might be faster than extract?
         uint32_t buffer[sizeof(__m256i) / sizeof(rle16_t)];
-        _mm256_store_si256((__m256i *)buffer, total);
+        _mm256_storeu_si256((__m256i *)buffer, total);
         sum += (buffer[0] + buffer[1]) + (buffer[2] + buffer[3]) +
                (buffer[4] + buffer[5]) + (buffer[6] + buffer[7]);
     }
