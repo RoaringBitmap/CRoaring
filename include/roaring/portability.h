61b09de9cec0a2cfd055fda33efaf6ba7f98f781
Richard Odenweller <rodenweller@axcient.com>
2018-11-02 07:19:34 -0600

    Adding correct version of file.

--- include/roaring/portability.h
+++ include/roaring/portability.h
@@ -141,26 +141,28 @@ static inline int __builtin_clzll(unsigned long long input_num) {
 
 /* result might be undefined when input_num is zero */
 #ifdef USESSE4
+/* POPCNT support was added to processors around the release of SSE4.2 */
+/* USESSE4 flag guarantees POPCNT support */
 static inline int __builtin_popcountll(unsigned long long input_num) {
 #ifdef _WIN64  // highly recommended!!!
-    return (int)__popcnt64(input_num);
+	return (int)__popcnt64(input_num);
 #else  // if we must support 32-bit Windows
-    return (int)(__popcnt((uint32_t)input_num) +
-                 __popcnt((uint32_t)(input_num >> 32)));
+	return (int)(__popcnt((uint32_t)input_num) +
+		__popcnt((uint32_t)(input_num >> 32)));
 #endif
 }
 #else
-/* avoids use of popcnt which is unsupported on old processors */
+/* software implementation avoids POPCNT */
 static inline int __builtin_popcountll(unsigned long long input_num) {
 	const uint64_t m1 = 0x5555555555555555; //binary: 0101...
 	const uint64_t m2 = 0x3333333333333333; //binary: 00110011..
 	const uint64_t m4 = 0x0f0f0f0f0f0f0f0f; //binary:  4 zeros,  4 ones ...
 	const uint64_t h01 = 0x0101010101010101; //the sum of 256 to the power of 0,1,2,3...
 
-	x -= (x >> 1) & m1;             //put count of each 2 bits into those 2 bits
-	x = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits 
-	x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits 
-	return (x * h01) >> 56;			//returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ... 
+	input_num -= (input_num >> 1) & m1;
+	input_num = (input_num & m2) + ((input_num >> 2) & m2);
+	input_num = (input_num + (input_num >> 4)) & m4;
+	return (input_num * h01) >> 56;
 }
 #endif
 
