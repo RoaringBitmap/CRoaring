243983dd912ea39fc2322aaaf10d86b018362b17
Cerul Alain <ae@metaeducation.com>
2020-10-31 08:42:42 -0400

    Use private/public separation in amalgamation.sh
    Though the export dependencies for roaring.h and roaring.hh were
    reduced, the amalgamation script put all the header files into
    the aggregated roaring.h -- whether they were public or private.
    
    This separates the headers out, so that the private ones are
    packed into the .C file instead of the .H file.
    
    The .HH file is also adjusted to use the #define guard that keeps
    the C API from being put in the global namespace by default when
    it re-adds the `#include roaring.h` that it stripped out.
    
    Minor changes to reduce redundancy are included--factoring the
    timestamp and generated file warning into a function, an omitting
    the original-file-location comments when including the license.

--- amalgamation.sh
+++ amalgamation.sh
@@ -5,23 +5,31 @@
 ########################################################################
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+timestamp=$(date)  # capture to label files with their generation time
+
 echo "We are about to amalgamate all CRoaring files into one source file. "
 echo "See https://www.sqlite.org/amalgamation.html and https://en.wikipedia.org/wiki/Single_Compilation_Unit for rationale. "
 
 AMAL_H="roaring.h"
 AMAL_C="roaring.c"
 
-# order does not matter
-ALLCFILES=$( ( [ -d $SCRIPTPATH/.git ] && ( type git >/dev/null 2>&1 ) &&  ( git ls-files $SCRIPTPATH/src/*.c $SCRIPTPATH/src/**/*c ) ) ||  ( find $SCRIPTPATH/src -name '*.c' ) )
-
 # order matters
-ALLCHEADERS="
-$SCRIPTPATH/src/license-comment.h
+ALL_PUBLIC_H="
 $SCRIPTPATH/include/roaring/roaring_version.h
+$SCRIPTPATH/include/roaring/roaring_types.h
+$SCRIPTPATH/include/roaring/roaring.h
+"
+
+# order does not matter
+ALL_PRIVATE_C=$( ( [ -d $SCRIPTPATH/.git ] && ( type git >/dev/null 2>&1 ) &&  ( git ls-files $SCRIPTPATH/src/*.c $SCRIPTPATH/src/**/*c ) ) ||  ( find $SCRIPTPATH/src -name '*.c' ) )
+
+# these private header files are embedded at the head of the amalgamated C file
+# the amalgamated public header file must be #include'd before the private ones
+# order matters
+ALL_PRIVATE_H="
 $SCRIPTPATH/include/roaring/portability.h
 $SCRIPTPATH/include/roaring/containers/perfparameters.h
 $SCRIPTPATH/include/roaring/array_util.h
-$SCRIPTPATH/include/roaring/roaring_types.h
 $SCRIPTPATH/include/roaring/utilasm.h
 $SCRIPTPATH/include/roaring/bitset_util.h
 $SCRIPTPATH/include/roaring/containers/array.h
@@ -38,20 +46,32 @@ $SCRIPTPATH/include/roaring/containers/mixed_xor.h
 $SCRIPTPATH/include/roaring/containers/containers.h
 $SCRIPTPATH/include/roaring/roaring_array.h
 $SCRIPTPATH/include/roaring/misc/configreport.h
-$SCRIPTPATH/include/roaring/roaring.h
 "
 
-for i in ${ALLCHEADERS} ${ALLCFILES}; do
+for i in ${ALL_PUBLIC_H} ${ALL_PRIVATE_C} ${ALL_PUBLIC_H}; do
     test -e $i && continue
     echo "FATAL: source file [$i] not found."
     exit 127
 done
 
+function echo_timestamp()
+{
+    echo "// !!! DO NOT EDIT - THIS IS AN AUTO-GENERATED FILE !!!"
+    echo "// Created by amalgamation.sh on ${timestamp}"
+    echo ""  # newline
+}
+
+function echo_license()
+{
+    cat $SCRIPTPATH/src/license-comment.h
+    echo ""  # newline
+}
 
 function stripinc()
 {
     sed -e '/# *include *"/d' -e '/# *include *<roaring\//d'
 }
+
 function dofile()
 {
     RELFILE=${1#"$SCRIPTPATH/"}
@@ -61,19 +81,22 @@ function dofile()
     echo "/* end file $RELFILE */"
 }
 
-timestamp=$(date)
 echo "Creating ${AMAL_H}..."
-echo "/* auto-generated on ${timestamp}. Do not edit! */" > "${AMAL_H}"
 {
-    for h in ${ALLCHEADERS}; do
+    echo_timestamp
+    echo_license
+
+    for h in ${ALL_PUBLIC_H}; do
         dofile $h
     done
-} >> "${AMAL_H}"
+} > "${AMAL_H}"
 
 
 echo "Creating ${AMAL_C}..."
-echo "/* auto-generated on ${timestamp}. Do not edit! */" > "${AMAL_C}"
 {
+    echo_timestamp
+    echo_license
+  
     echo "#include \"${AMAL_H}\""
 
     echo ""
@@ -83,18 +106,20 @@ echo "/* auto-generated on ${timestamp}. Do not edit! */" > "${AMAL_C}"
     echo "#endif"
     echo ""
 
-    for h in "$SCRIPTPATH/src/license-comment.h" ${ALLCFILES}; do
+    echo "#include \"roaring.h\"  /* include public API definitions */"
+
+    for h in ${ALL_PRIVATE_H} ${ALL_PRIVATE_C}; do
         dofile $h
     done
-} >> "${AMAL_C}"
-
-
+} > "${AMAL_C}"
 
 
 DEMOC="amalgamation_demo.c"
 echo "Creating ${DEMOC}..."
-echo "/* auto-generated on ${timestamp}. Do not edit! */" > "${DEMOC}"
-cat <<< '
+{
+    echo_timestamp
+
+    cat <<< '
 #include <stdio.h>
 #include "roaring.c"
 int main() {
@@ -104,7 +129,8 @@ int main() {
   roaring_bitmap_free(r1);
   return 0;
 }
-' >>  "${DEMOC}"
+'
+} > "${DEMOC}"
 
 
 echo "Done with C amalgamation. Proceeding with C++ wrap."
@@ -112,21 +138,37 @@ echo "Done with C amalgamation. Proceeding with C++ wrap."
 AMAL_HH="roaring.hh"
 
 echo "Creating ${AMAL_HH}..."
-ALLCPPHEADERS="$SCRIPTPATH/cpp/roaring.hh $SCRIPTPATH/cpp/roaring64map.hh"
-echo "/* auto-generated on ${timestamp}. Do not edit! */" > "${AMAL_HH}"
+ALL_PUBLIC_HH="$SCRIPTPATH/cpp/roaring.hh $SCRIPTPATH/cpp/roaring64map.hh"
 {
-    echo "#include \"${AMAL_H}\""
+    echo_timestamp
+    echo_license
 
-    for h in "$SCRIPTPATH/src/license-comment.h" ${ALLCPPHEADERS}; do
-        dofile $h
+    # using the C++ roaring:: namespace does not make the C API available by
+    # default.  One must either `#include "roaring.h"` manually or use the
+    # `roaring::api::` namespace.  The protection against putting the API in
+    # global scope is one via a #define surrounding the include, but that
+    # inclusion is stripped out so we repeat it here.
+    #
+    # !!! This would be better if it found the inclusion of roaring.h and
+    # replaced it with the amalgamated file.
+    #
+    echo "#define ROARING_API_NOT_IN_GLOBAL_NAMESPACE  // see remarks in roaring.h"
+    echo "#include \"${AMAL_H}\""
+    echo "#undef ROARING_API_NOT_IN_GLOBAL_NAMESPACE"
+
+    for hh in ${ALL_PUBLIC_HH}; do
+        dofile $hh
     done
-} >> "${AMAL_HH}"
+} > "${AMAL_HH}"
 
 
 DEMOCPP="amalgamation_demo.cpp"
 echo "Creating ${DEMOCPP}..."
-echo "/* auto-generated on ${timestamp}. Do not edit! */" > "${DEMOCPP}"
-cat <<< '
+{
+    echo_timestamp
+    echo_license
+
+    cat <<< '
 #include <iostream>
 #include "roaring.hh"
 #include "roaring.c"
@@ -144,7 +186,8 @@ int main() {
   std::cout << "cardinality = " << r2.cardinality() << std::endl;
   return 0;
 }
-' >>  "${DEMOCPP}"
+'
+} >  "${DEMOCPP}"
 echo "Done with C++."
 
 echo "Done with all files generation. "
