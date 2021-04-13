396539d9fc50030f71630afce9551119d7c2b0a0
Daniel Lemire <lemire@gmail.com>
2021-04-13 13:16:25 -0400

    Cleaning the CMake.

--- tools/cmake/FindOptions.cmake
+++ tools/cmake/FindOptions.cmake
@@ -12,21 +12,9 @@ if(ROARING_SANITIZE)
   endif()
 endif()
 
-## -march=native is not supported on some platforms
-if(NOT MSVC)
-
-if(NOT ROARING_DISABLE_NATIVE)
-set(OPT_FLAGS "-march=${ROARING_ARCH}")
-endif()
-endif()
-
 if(ROARING_DISABLE_X64)
   # we can manually disable any optimization for x64
   set (OPT_FLAGS "${OPT_FLAGS} -DDISABLE_X64" )
-endif()
-if(ROARING_DISABLE_AVX)
-  # we can manually disable AVX by defining DISABLEAVX
-  set (OPT_FLAGS "${OPT_FLAGS} -DDISABLEAVX" )
 endif()
 if(ROARING_DISABLE_NEON)
   set (OPT_FLAGS "${OPT_FLAGS} -DDISABLENEON" )
