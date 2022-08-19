3452a8b05db7d0c2990002c3bec164dab50fd654
Zachary Dremann <dremann@gmail.com>
2022-08-19 17:12:30 -0400

    Posix is a lie

--- benchmarks/benchmark.h
+++ benchmarks/benchmark.h
@@ -37,34 +37,31 @@
         (cycles) = ((uint64_t)cyc_high << 32) | cyc_low;                      \
     } while (0)
 
-#elif _POSIX_C_SOURCE >= 199309L // Posix check for clock_gettime
+#else  // defined(CROARING_IS_X64) && defined(ROARING_INLINE_ASM)
 
-#include <time.h>
-
-#if defined(CLOCK_THREAD_CPUTIME_ID) || _POSIX_THREAD_CPUTIME
+#if defined(CLOCK_THREAD_CPUTIME_ID)
 #define RDTSC_CLOCK_ID CLOCK_THREAD_CPUTIME_ID
 #elif defined(CLOCK_MONOTONIC)
 #define RDTSC_CLOCK_ID CLOCK_MONOTONIC
-#else
+#elif defined(CLOCK_REALTIME)
 #define RDTSC_CLOCK_ID CLOCK_REALTIME
 #endif
 
-#define RDTSC_START(cycles) \
-  do { \
-    struct timespec ts; \
-    clock_gettime(RDTSC_CLOCK_ID, &ts); \
-    cycles = ts.tv_sec * UINT64_C(1000000000) + ts.tv_nsec; \
-  } while (0)
+#if defined(RDTSC_CLOCK_ID)
+#define RDTSC_START(cycles)                                     \
+    do {                                                        \
+        struct timespec ts;                                     \
+        clock_gettime(RDTSC_CLOCK_ID, &ts);                     \
+        cycles = ts.tv_sec * UINT64_C(1000000000) + ts.tv_nsec; \
+    } while (0)
 
 #define RDTSC_FINAL(cycles) RDTSC_START(cycles)
 
-#else
+#else  // defined(RDTSC_CLOCK_ID)
 
 /**
-* Other architectures do not support rdtsc ?
+* Fall back to the `clock` function
 */
-#include <time.h>
-
 #define RDTSC_START(cycles) \
     do {                    \
         cycles = clock();   \
@@ -72,6 +69,7 @@
 
 #define RDTSC_FINAL(cycles) RDTSC_START(cycles)
 
+#endif
 #endif
 
 /*
