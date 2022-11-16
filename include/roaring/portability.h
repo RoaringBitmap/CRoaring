4351a6cf8ee7f0b17bd18386bf43c503fc555d51
Daniel Lemire <daniel@lemire.me>
2022-11-16 17:19:53 -0500

    Let us guard the malloc.h include with a check for glibc. (#412)

--- include/roaring/portability.h
+++ include/roaring/portability.h
@@ -49,7 +49,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>  // will provide posix_memalign with _POSIX_C_SOURCE as defined above
-#if !(defined(__APPLE__)) && !(defined(__FreeBSD__))
+#ifdef __GLIBC__
 #include <malloc.h>  // this should never be needed but there are some reports that it is needed.
 #endif
 
