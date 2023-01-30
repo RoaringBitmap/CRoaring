5f64aba8303815cc274b152d7745f7bbeb1080fe
Daniel Lemire <daniel@lemire.me>
2023-01-30 14:59:58 -0500

    [skip-ci] doc update

--- tools/prepare_doxygen.sh
+++ tools/prepare_doxygen.sh
@@ -5,7 +5,7 @@ PACKAGE_URL="https://github.com/jothepro/doxygen-awesome-css.git"
 PACKAGE_VERSION="v2.1.0"
 
 BASE_DIR=$(pwd)
-THEME_DIR="$BASE_DIR/docs/theme"
+THEME_DIR="$BASE_DIR/theme"
 WORKSPACE=$(mktemp -d 2> /dev/null || mktemp -d -t 'tmp')
 cleanup () {
   EXIT_CODE=$?
@@ -18,5 +18,4 @@ trap cleanup INT TERM EXIT
 cd "$WORKSPACE"
 git clone --depth=1 --branch "$PACKAGE_VERSION" "$PACKAGE_URL" theme
 rm -rf "$THEME_DIR"
-mkdir -p "$THEME_DIR"
 mv "$WORKSPACE/theme" "$THEME_DIR"
