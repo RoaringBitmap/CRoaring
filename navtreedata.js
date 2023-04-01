700f34f84b1dc73a0b70a17525b2279d3ec50b2d
lemire <lemire@users.noreply.github.com>
2023-04-01 23:20:58 +0000

    deploy: 0eafc44b6f83522ab158676dd3553cbcd32e2eb3

--- navtreedata.js
+++ navtreedata.js
@@ -32,31 +32,32 @@ var NAVTREE =
     [ "Amalgamation/Unity Build", "index.html#autotoc_md5", null ],
     [ "API", "index.html#autotoc_md6", null ],
     [ "Dealing with large volumes", "index.html#autotoc_md7", null ],
-    [ "Custom memory allocators", "index.html#autotoc_md8", null ],
-    [ "Example (C)", "index.html#autotoc_md9", null ],
-    [ "Conventional bitsets (C)", "index.html#autotoc_md10", null ],
-    [ "Example (C++)", "index.html#autotoc_md11", null ],
-    [ "Building with cmake (Linux and macOS, Visual Studio users should see below)", "index.html#autotoc_md12", null ],
-    [ "Building (Visual Studio under Windows)", "index.html#autotoc_md13", [
-      [ "Usage (Using <tt>conan</tt>)", "index.html#autotoc_md14", null ],
-      [ "Usage (Using <tt>vcpkg</tt> on Windows, Linux and macOS)", "index.html#autotoc_md15", null ]
+    [ "Running microbenchmarks", "index.html#autotoc_md8", null ],
+    [ "Custom memory allocators", "index.html#autotoc_md9", null ],
+    [ "Example (C)", "index.html#autotoc_md10", null ],
+    [ "Conventional bitsets (C)", "index.html#autotoc_md11", null ],
+    [ "Example (C++)", "index.html#autotoc_md12", null ],
+    [ "Building with cmake (Linux and macOS, Visual Studio users should see below)", "index.html#autotoc_md13", null ],
+    [ "Building (Visual Studio under Windows)", "index.html#autotoc_md14", [
+      [ "Usage (Using <tt>conan</tt>)", "index.html#autotoc_md15", null ],
+      [ "Usage (Using <tt>vcpkg</tt> on Windows, Linux and macOS)", "index.html#autotoc_md16", null ]
     ] ],
-    [ "SIMD-related throttling", "index.html#autotoc_md16", null ],
-    [ "Thread safety", "index.html#autotoc_md17", null ],
-    [ "How to best aggregate bitmaps?", "index.html#autotoc_md18", null ],
-    [ "Wrappers", "index.html#autotoc_md19", [
-      [ "Python", "index.html#autotoc_md20", null ],
-      [ "JavaScript", "index.html#autotoc_md21", null ],
-      [ "Swift", "index.html#autotoc_md22", null ],
-      [ "C", "index.html#autotoc_md23", null ],
-      [ "Go (golang)", "index.html#autotoc_md24", null ],
-      [ "Rust", "index.html#autotoc_md25", null ],
-      [ "D", "index.html#autotoc_md26", null ],
-      [ "Redis", "index.html#autotoc_md27", null ],
-      [ "Zig", "index.html#autotoc_md28", null ]
+    [ "SIMD-related throttling", "index.html#autotoc_md17", null ],
+    [ "Thread safety", "index.html#autotoc_md18", null ],
+    [ "How to best aggregate bitmaps?", "index.html#autotoc_md19", null ],
+    [ "Wrappers", "index.html#autotoc_md20", [
+      [ "Python", "index.html#autotoc_md21", null ],
+      [ "JavaScript", "index.html#autotoc_md22", null ],
+      [ "Swift", "index.html#autotoc_md23", null ],
+      [ "C", "index.html#autotoc_md24", null ],
+      [ "Go (golang)", "index.html#autotoc_md25", null ],
+      [ "Rust", "index.html#autotoc_md26", null ],
+      [ "D", "index.html#autotoc_md27", null ],
+      [ "Redis", "index.html#autotoc_md28", null ],
+      [ "Zig", "index.html#autotoc_md29", null ]
     ] ],
-    [ "Mailing list/discussion group", "index.html#autotoc_md29", null ],
-    [ "References about Roaring", "index.html#autotoc_md30", null ],
+    [ "Mailing list/discussion group", "index.html#autotoc_md30", null ],
+    [ "References about Roaring", "index.html#autotoc_md31", null ],
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ]
     ] ],
@@ -85,7 +86,7 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "annotated.html",
-"index.html#autotoc_md7"
+"index.html#autotoc_md6"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- navtreeindex1.js
+++ navtreeindex1.js
@@ -1,8 +1,9 @@
 var NAVTREEINDEX1 =
 {
+"index.html#autotoc_md6":[5],
 "index.html#autotoc_md7":[6],
 "index.html#autotoc_md8":[7],
 "index.html#autotoc_md9":[8],
-"namespaces.html":[19,0],
+"namespaces.html":[20,0],
 "pages.html":[]
 };
