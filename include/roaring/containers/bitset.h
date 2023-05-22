41d56f291ffe95e569b4f1dc9de4e73b89aced99
Taylor Blau <me@ttaylorr.com>
2023-05-22 16:32:51 -0400

    containers/bitset.h: declare `bitset_container_intersection_nocard()`
    In a similar spirit as the previous commits, declare a function
    prototype for `bitset_container_intersection_nocard()` which rounds out
    the existing set of function prototypes for the macro expansion of:
    
        BITSET_CONTAINER_FN(intersection, &, _mm256_and_si256, vandq_u64)
    
    Signed-off-by: Taylor Blau <me@ttaylorr.com>

--- include/roaring/containers/bitset.h
+++ include/roaring/containers/bitset.h
@@ -340,6 +340,12 @@ int bitset_container_intersection_justcard(const bitset_container_t *src_1,
 
 /* Computes the intersection of bitsets `src_1' and `src_2' into `dst', but does
  * not update the cardinality. Provided to optimize chained operations. */
+int bitset_container_intersection_nocard(const bitset_container_t *src_1,
+					 const bitset_container_t *src_2,
+					 bitset_container_t *dst);
+
+/* Computes the intersection of bitsets `src_1' and `src_2' into `dst', but does
+ * not update the cardinality. Provided to optimize chained operations. */
 int bitset_container_and_nocard(const bitset_container_t *src_1,
                                 const bitset_container_t *src_2,
                                 bitset_container_t *dst);
