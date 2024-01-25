61ab82b725946e1d2df77c854cdedc428d1ef94d
Daniel Lemire <daniel@lemire.me>
2024-01-25 12:17:44 -0500

    Testing amalgamation files

--- amalgamation.sh
+++ amalgamation.sh
@@ -174,6 +174,25 @@ echo "Creating ${DEMOC}..."
 #include <stdio.h>
 #include <stdlib.h>
 #include "roaring.c"
+
+
+void or_many(void) {
+    roaring_bitmap_t *r1 = roaring_bitmap_of(2, 500, 1000);
+    roaring_bitmap_t *r2 = roaring_bitmap_of(2, 1000, 2000);
+
+    const roaring_bitmap_t *bitmap_arr[2] = {r1, r2};
+    fprintf(stderr, "Going to or many\n");
+    for (int i = 0; i < 10000; i++) {
+        roaring_bitmap_t *r = roaring_bitmap_or_many(2, bitmap_arr);
+        roaring_bitmap_free(r);
+    }
+
+    fprintf(stderr, "Got done\n");
+
+    roaring_bitmap_free(r2);
+    roaring_bitmap_free(r1);
+}
+
 int main() {
   roaring_bitmap_t *r1 = roaring_bitmap_create();
   for (uint32_t i = 100; i < 1000; i++) roaring_bitmap_add(r1, i);
@@ -191,6 +210,7 @@ int main() {
   }
   printf("%zu \n", bitset_count(b));
   bitset_free(b);
+  or_many();
   return EXIT_SUCCESS;
 }
 '
