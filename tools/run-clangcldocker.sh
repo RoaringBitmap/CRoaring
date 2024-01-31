b489cbaf8407211a42caa6742d4f8d0e1d449949
Daniel Lemire <dlemire@lemire.me>
2024-01-31 17:38:44 -0500

    simpler version

--- tools/run-clangcldocker.sh
+++ tools/run-clangcldocker.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 set -e
-set -o noglob
 COMMAND=$@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 MAINSOURCE=$SCRIPTPATH/..
 ALL_CROARING_FILES=$(cd $MAINSOURCE && git ls-tree --full-tree --name-only -r HEAD | grep -e ".*\.\(c\|h\|cc\|cpp\|hh\)\$" | grep -vFf clang-format-ignore.txt)
-tuser=$(echo $USER | tr -dc 'a-z')
 
-container_name=${CONTAINER_NAME:-"clang-format-for-$tuser"}
-echo $container_name
-
+if clang-format-17 --version | grep -qF 'version 17.'; then
+  clang-format-17 "$@"
+  exit 0
+elif clang-format --version | grep -qF 'version 17.'; then
+  clang_format=( clang-format )
+  clang-format "$@"
+  exit 0
+fi
+echo "Trying to use docker"
 command -v docker >/dev/null 2>&1 || { echo >&2 "Please install docker. E.g., go to https://www.docker.com/products/docker-desktop Type 'docker' to diagnose the problem."; exit 1; }
-
 docker info >/dev/null 2>&1 || { echo >&2 "Docker server is not running? type 'docker info'."; exit 1; }
 
-docker image inspect $container_name >/dev/null 2>&1 || ( echo "instantiating the container" ; docker build --no-cache -t $container_name -f $SCRIPTPATH/clangcldockerfile --build-arg USER_NAME="$tuser"  --build-arg USER_ID=$(id -u)  --build-arg GROUP_ID=$(id -g) .  )
-
 if [ -t 0 ]; then DOCKER_ARGS=-it; fi
-docker run --rm $DOCKER_ARGS -h $container_name -v $MAINSOURCE:$MAINSOURCE:Z  -w $MAINSOURCE $container_name sh --style=file --verbose -i $ALL_CROARING_FILES $COMMAND
+docker pull kszonek/clang-format-17
 
+docker run --rm $DOCKER_ARGS -v "$MAINSOURCE":"$MAINSOURCE":Z  -w "$MAINSOURCE" -u "$(id -u $USER):$(id -g $USER)" kszonek/clang-format-17 --style=file --verbose -i "$@" $ALL_CROARING_FILES
 
 
