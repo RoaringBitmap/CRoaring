98060934f656b471d1b1a0d132057c5d6858f353
Daniel Lemire <daniel@lemire.me>
2024-02-02 14:45:38 -0500

    suppressing command not found

--- tools/run-clangcldocker.sh
+++ tools/run-clangcldocker.sh
@@ -5,10 +5,10 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 MAINSOURCE=$SCRIPTPATH/..
 ALL_CROARING_FILES=$(cd $MAINSOURCE && git ls-tree --full-tree --name-only -r HEAD | grep -e ".*\.\(c\|h\|cc\|cpp\|hh\)\$" | grep -vFf clang-format-ignore.txt)
 
-if clang-format-17 --version | grep -qF 'version 17.'; then
+if clang-format-17 --version  2>/dev/null | grep -qF 'version 17.'; then
   clang-format-17 --style=file --verbose -i "$@" $ALL_CROARING_FILES
   exit 0
-elif clang-format --version | grep -qF 'version 17.'; then
+elif clang-format --version  2>/dev/null | grep -qF 'version 17.'; then
   clang-format --style=file --verbose -i "$@" $ALL_CROARING_FILES
   exit 0
 fi
