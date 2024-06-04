857c3208b49ea2b998caee2dcdb39eb2f66d5159
Paul Smith <paul@mad-scientist.net>
2024-06-04 08:42:17 -0400

    roaring64: Don't crash on roaring64_bitmap_free(NULL) (#632)

--- src/roaring64.c
+++ src/roaring64.c
@@ -148,6 +148,9 @@ roaring64_bitmap_t *roaring64_bitmap_create(void) {
 }
 
 void roaring64_bitmap_free(roaring64_bitmap_t *r) {
+    if (!r) {
+        return;
+    }
     art_iterator_t it = art_init_iterator(&r->art, /*first=*/true);
     while (it.value != NULL) {
         leaf_t *leaf = (leaf_t *)it.value;
