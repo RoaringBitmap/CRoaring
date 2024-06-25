344aa48519892d51ec1ab5dbe8b031051f8feb16
Zachary Dremann <dremann@gmail.com>
2024-06-25 15:35:53 -0400

    Mark `sum_value` in roaring_statistics_t as deprecated

--- amalgamation.sh
+++ amalgamation.sh
@@ -36,8 +36,8 @@ DEMOCPP="amalgamation_demo.cpp"
 #
 ALL_PUBLIC_H="
 $SCRIPTPATH/include/roaring/roaring_version.h
-$SCRIPTPATH/include/roaring/roaring_types.h
 $SCRIPTPATH/include/roaring/portability.h
+$SCRIPTPATH/include/roaring/roaring_types.h
 $SCRIPTPATH/include/roaring/bitset/bitset.h
 $SCRIPTPATH/include/roaring/roaring.h
 $SCRIPTPATH/include/roaring/memory.h
